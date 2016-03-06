df869ecee5fbe8348ec22799007fd33d650c2acf
Shylock.Gou <goudezong@gmail.com>
2016-03-06 16:41:57 +0800

    dns

--- unbound/start.sh
+++ unbound/start.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+docker run --name dns -d -p 53:53/udp -p 53:53/tcp -v /root/Workspaces/docker/unbound/unbound:/etc/unbound dns
 docker run --name dns -d -v /home/shylock/Workspaces/docker/unbound/unbound:/etc/unbound dns
