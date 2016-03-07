3a10b13bd8258b1426f8c96ac73e7c55f85ede4d
Shylock.Gou <goudezong@gmail.com>
2016-03-08 01:57:04 +0800

    key

--- nginx/ssl/slef-cert.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-openssl req -config input.conf -new -x509 -sha256 -newkey rsa:2048 -nodes \
-        -keyout example.key.pem -days 365 -out example.cert.pem
-
-openssl req -config input.conf -new -sha256 -newkey rsa:2048 -nodes \
-        -keyout example.key -days 365 -out example.crt
